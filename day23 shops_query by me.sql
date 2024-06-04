1c1f9e6f1ec940c8dfcd905c6d7d0ca056a94814
shraddhaakhuraswar <shraddhak2312@gmail.com>
2024-06-05 02:18:16 +0530

    sql task 1 commit

---
+++ day23 shops_query by me.sql
@@ -0,0 +1,121 @@
+create table shops(
+  id int primary key,
+	name varchar(150),
+	no_of_items int,
+	category varchar(15),
+	no_of_emp int,
+	owner_name varchar(150),
+	address varchar(150),
+	mobile_no int,
+	sales int,
+	revenue int,
+	is_active boolean
+	
+)
+select * from shops
+	
+insert into shops values 
+(01, 'mobile',100,'retail',20,'alice ','MG Road',978654,200000,10000,true ),
+(02, 'baker delight',50,'retail',20,'bob','Brigade Road',654789,1200000,600000,true ),
+(03,'green grocers',500,'online',20,'david','Anna Salai, Chennai, Tamil Nadu',963258,500000,60000,true ),
+(04,'tech haven',90,'retail',20,'catherine','Koramangala, Bangalore, Karnataka',741258,130000,96300,true ),
+(05,'fashion hub',100,'wholesale',20,'emma','Greater Kailash, New Delhi',9632874,410000,95000,true ),
+(06,'toyland',500,'retail',20,'grace',' Park Street, Kolkata, West Bengal',96325874,963200,20096,true ),
+(07,'book nook',200,'online',20,'ivy',' MG Road, Pune, Maharashtra',965412,121096,20054,true ),
+(08,'coffee corner',40,'retail',20,'leo',' Linking Road, Mumbai, Maharashtra',9652,120022,28520,true ),
+(09,'pet paradise',100,'retail',20,'jack',' Church Street, Bangalore, Karnataka',8945120,12300,9630,true ),
+(10,'flower station',20,'retail',20,'karen',' Nungambakkam High Road, Chennai, Tamil Nadu',742365,1009654,206540,true ),
+(11,'gadget world',300,'retail',20,'monaa',' Camac Street, Kolkata, West Bengal',8963,165110,20051,true ),
+(12,'sports gear',250,'retail',20,'nate','Sector 17, Chandigarh',645450,199600,295412,true ),
+(13,'beauty bliss',400,'online',20,'olivia',' CP Ramaswamy Road, Chennai, Tamil Nadu',945080,146300,28800,true ),	
+(14,'vintage wines',80,'retail',20,'paul','Bandra West, Mumbai, Maharashtra',9452,983000,74100,true ),
+(18,'bike barn',100,'retail',20,'rachel','Residency Road, Bangalore',987452,198300,2654000,true ),
+(16,'art emporium',60,'retail',20,'steve',' Shakespeare Sarani, Kolkata, West Bengal',984563,128400,98000,true ),
+(17,'music mania',100,'retail',20,'tinna',' Nehru Place, New Delhi',954258,1852000,29630,false ),
+(19,'toy kingdomn',110,'online',20,'zane',' Gariahat, Kolkata, West Bengal',96380,987000,125470,true ),
+(20,'health hub',103,'retail',20,'adam',' Koramangala, Bangalore, Karnataka',86540,184000,954220,false )
+insert into shops values	
+(21,'kitchen corner',160,'wholesale',20,'carl','Hauz Khas, New Delhi',8963240,10845210,209631,true ),
+(22,'fashion fiesta',106,'online',20,'george','Kothrud, Pune, Maharashtra',896004,199540,640099,true ),
+(23,'outdoor outfitters',190,'retail',20,'holly',' South Extension, New Delhi',8096541,100000,20000,true ),
+(24,'shoe central',120,'retail',20,'jill','Banjara Hills, Hyderabad, Telangana',74180,100000,20000,true ),
+(25,'bag boutique',130,'online',20,'kyle','Andheri East, Mumbai, Maharashtra',89683,165400,285244,true ),
+(26,'jewelry junction',160,'online',20,'mike',' MG Road, Gurgaon, Haryana',987456,109654,2852147,true ),
+(27,'stationery store',155,'retail',20,'lara',' Rajouri Garden',894520,100745,9632587,true ),
+(28,'furniture depot',175,'retail',20,'naina',' Connaught Place, Delhi',8080,100000,20000,true ),
+(29,'party supplies',125,'retail',20,'rita','Lajpat Nagar,Delhi',8080,100000,20000,true ),
+(30,'mobile',40,'retail',20,'samm','Vashi, Navi Mumbai, Maharashtra',8080,100000,20000,true ),
+(31,'gaming galaxy',640,'retail',20,'taraa','Lalbagh Road, Bangalore, Karnataka',8080,100000,20000,true ),
+(33,'mobile master',200,'retail',20,'kim',' Jubilee Hills, Hyderabad, Telangana',8080,100000,20000,true ),
+(34,'tool town',120,'retail',20,'lee','Vashi, Navi Mumbai, Maharashtra',8080,100000,20000,true ),
+(35,'kitchen kingdom',160,'retail',20,'jon','Salt Lake, Kolkata, West Bengal',8080,100000,20000,true ),
+(36,'gift gallery',100,'online',20,'criss','Jayanagar, Bangalore, Karnataka',8080,100000,20000,true ),
+(38,'tea shop ',30,'retail',20,'xena','Indiranagar, Bangalore, Karnataka',8080,100000,20000,true ),
+(40,'car shop',100,'retail',20,'rocky','Jayanagar, Bangalore, Karnataka',8080,100000,20000,true )
+insert into shops values 
+(1, 'mobile',100,'retail',20,'alice ','MG Road',978654,200000,10000,true )
+(2, 'baker delight',50,'retail',20,'bob','Brigade Road',654789,1200000,600000,true ),
+(3,'green grocers',500,'online',20,'david','Anna Salai, Chennai, Tamil Nadu',963258,500000,60000,true ),
+(4,'tech haven',90,'retail',20,'catherine','Koramangala, Bangalore, Karnataka',741258,130000,96300,true )
+(5,'fashion hub',1000,'wholesale',20,'emma','Greater Kailash, New Delhi',96325874,410000,95000,true ),
+(6,'toyland',500,'retail',20,'grace',' Park Street, Kolkata, West Bengal',96325874,963200,20096,true ),
+(7,'book nook',200,'online',20,'ivy',' MG Road, Pune, Maharashtra',965412,121096,20054,true ),
+(8,'coffee corner',40,'retail',20,'leo',' Linking Road, Mumbai, Maharashtra',9652,120022,28520,true ),
+(9,'pet paradise',100,'retail',20,'jack',' Church Street, Bangalore, Karnataka',8945120,12300,9630,true ),
+(10,'flower station',20,'retail',20,'karen',' Nungambakkam High Road, Chennai, Tamil Nadu',742365,1009654,206540,true ),
+(11,'gadget world',300,'retail',20,'monaa',' Camac Street, Kolkata, West Bengal',8963,165110,20051,true ),
+(12,'sports gear',250,'retail',20,'nate','Sector 17, Chandigarh',645450,199600,295412,true ),
+(13,'beauty bliss',400,'online',20,'olivia',' CP Ramaswamy Road, Chennai, Tamil Nadu',945080,146300,28800,true ),
+(14,'vintage wines',80,'retail',20,'paul','Bandra West, Mumbai, Maharashtra',9452,963000,74100,true ),
+(15,'bike barn',100,'retail',20,'rachel','Residency Road, Bangalore, Karnataka',987452,196300,2654000,true ),
+(16,'art emporium',60,'retail',20,'steve',' Shakespeare Sarani, Kolkata, West Bengal',984563,125400,98000,true ),
+(17,'music mania',100,'retail',20,'tinna',' Nehru Place, New Delhi',954258,185200,29630,true ),
+(19,'toy kingdomn',110,'online',20,'zane',' Gariahat, Kolkata, West Bengal',96380,9874000,125470,true ),
+(20,'health hub',103,'retail',20,'adam',' Koramangala, Bangalore, Karnataka',86540,185000,954220,true ),
+(21,'kitchen corner',160,'wholesale',20,'carl','Hauz Khas, New Delhi',8963240,10845210,209631,true ),
+(22,'fashion fiesta',106,'online',20,'george','Kothrud, Pune, Maharashtra',896004,199540,640099,true )
+(23,'outdoor outfitters',190,'retail',20,'holly',' South Extension, New Delhi',8096541,100000,20000,true ),
+(24,'shoe central',120,'retail',20,'jill','Banjara Hills, Hyderabad, Telangana',74180,100000,20000,true ),
+(25,'bag boutique',130,'online',20,'kyle','Andheri East, Mumbai, Maharashtra',89683,165400,285244,true ),
+(26,'jewelry junction',160,'online',20,'mike',' MG Road, Gurgaon, Haryana',987456,109654,2852147,true ),
+(27,'stationery store',155,'retail',20,'lara',' Rajouri Garden',894520,100745,9632587,true ),
+(28,'furniture depot',175,'retail',20,'naina',' Connaught Place, Delhi',8080,100000,20000,true ),
+(29,'party supplies',125,'retail',20,'rita','Lajpat Nagar,Delhi',8080,100000,20000,true ),
+(30,'mobile',40,'retail',20,'samm','Vashi, Navi Mumbai, Maharashtra',8080,100000,20000,true ),
+(31,'gaming galaxy',640,'retail',20,'taraa','Lalbagh Road, Bangalore, Karnataka',8080,100000,20000,true ),
+(32,'craft corner',50,'retail',20,'tiaa',' Rajarhat, Kolkata, West Bengal',8080,100000,20000,true ),
+(33,'mobile master',200,'retail',20,'kim',' Jubilee Hills, Hyderabad, Telangana',8080,100000,20000,true ),
+(34,'tool town',120,'retail',20,'lee','Vashi, Navi Mumbai, Maharashtra',8080,100000,20000,true ),
+(35,'kitchen kingdom',160,'retail',20,'jon','Salt Lake, Kolkata, West Bengal',8080,100000,20000,true ),
+(36,'gift gallery',100,'online',20,'criss','Jayanagar, Bangalore, Karnataka',8080,100000,20000,true ),
+(37,'pet plaza',50,'retail',20,'veraa','FC Road, Pune, Maharashtra',8080,100000,20000,true ),
+(38,'tea shop ',30,'retail',20,'xena','Indiranagar, Bangalore, Karnataka',8080,100000,20000,true ),
+(39,'caffe',40,'retail',20,'alize',' FC Road, Pune, Maharashtra',8080,100000,20000,true ),
+(40,'car shop',100,'retail',20,'rocky','Jayanagar, Bangalore, Karnataka',8080,100000,20000,true )
+
+create table shop_items(
+	id serial primary key,
+	name varchar(150),
+	price int,
+	stock_qty int,
+	plucked_date date,
+	expire_within int,
+	shop_id int,
+	FOREIGN KEY (shop_id) references shops(id) 
+	
+)	
+insert into shop_items(name,price,stock_qty,plucked_date,expire_within,shop_id)
+	values
+	('onion',25,20,'30-06-2024',6,1),
+	('potato',25,30,'25-06-2024',7,2),
+	('dhaniya',30,20,'30-06-2024',4,3),
+	('tomato',30,20,'30-06-2024',4,4)
+	
+	
+select * from shops
+
+select * from shop_items
+
+	
+
+--category: 1) retails 2 )wholesale 3 )online
